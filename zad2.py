bfb705c0525e9460ccdefcde1c0f46dd79306e85
Adam Staciwa <01169353@pw.edu.pl>
2021-11-22 11:39:04 +0100

    lab6 v1.1

--- zad2.py
+++ zad2.py
@@ -4,6 +4,7 @@ def Funkcja2():
     if len(sys.argv) == 1:
         print("Podaj nazwe pliku w argumencie!")
     else:
+
         slownik = {}
         if os.path.isfile(sys.argv[1]):
             with open(sys.argv[1], 'r') as file:
@@ -28,6 +29,23 @@ def Funkcja2():
                     writer = csv.writer(file, delimiter=",")
                     for k, v in slownik.items():
                         writer.writerow([k, v])
+        else:
+            slownik = {}
+            next_user = "test"
+            password = ""
+
+            while next_user != "":
+                next_user = input("Podaj nazwe uzytkownika: ")
+                password = input("Podaj haslo: ")
+                if next_user != "":
+                    slownik[next_user] = password
+
+            print(slownik)
+
+            with open(sys.argv[1], "w") as file:
+                writer = csv.writer(file, delimiter=",")
+                for k, v in slownik.items():
+                    writer.writerow([k, v])
 
 
 
